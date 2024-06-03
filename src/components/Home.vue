4a4b5bdbcd8711f8b74b56ee1973d74bf84cf5d9
blackhole6 <45139071+blackhole6@users.noreply.github.com>
2024-06-03 22:20:03 +0800

    update: add request

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="home">
-    <h1>Home Page</h1>
+    <h1>HomePage</h1>
+    <div v-if="data">{{ data }}</div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  name: 'Home'
+  name: 'Home',
+  data() {
+    return {
+      data: null,
+    };
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get(' http://192.168.1.7:8080/api/data');
+        this.data = response.data.message;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
 }
 </script>
 
